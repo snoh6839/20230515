f7409ad112d73d4a8f721c98d0011e2e0f0351a2
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-17 23:40:48 +0900

    Update UserController.php

--- mini2/application/controller/UserController.php
+++ mini2/application/controller/UserController.php
@@ -90,4 +90,88 @@ class UserController extends Controller {
 
     }
 }
-?>
+// 변경후 코드 테스트전
+namespace application\controller;
+
+class UserController extends Controller {
+    public function loginGet() {
+        return "login" . _EXTENTION_PHP;
+    }
+
+    public function loginPost()
+    {
+        $result = $this->model->getUser($_POST);
+
+        if (count($result) === 0) {
+            $errMsg = "Invalid ID or PW";
+            $this->addDynamicProperty("errMsg", $errMsg);
+            return "login" . _EXTENTION_PHP;
+        }
+
+        $_SESSION[_STR_LOGIN_ID] = $_POST["id"];
+        return _BASE_REDIRECT . "/anime/main";
+    }
+
+    public function logoutGet()
+    {
+        session_unset();
+        session_destroy();
+        return "main" . _EXTENTION_PHP;
+    }
+
+    public function signupGet()
+    {
+        return "signup" . _EXTENTION_PHP;
+    }
+
+    public function signupPost()
+    {
+        $arrPost = $_POST;
+
+        $result = $this->model->getUserId($arrPost);
+        if (count($result) > 0) {
+            $errMsg = ": This User Already Exists. Please Login or Use Another ID";
+            $this->addDynamicProperty("errMsg", $errMsg);
+            return "signup" . _EXTENTION_PHP;
+        }
+
+        $name = $arrPost["name"];
+        if (!preg_match('/^[가-힣]{2,20}$/u', $name)) {
+            $errMsg = "Name must be between 2 and 20 characters long and can only contain Korean letters";
+            $this->addDynamicProperty("errMsg", $errMsg);
+            return "signup" . _EXTENTION_PHP;
+        }
+
+        $id = $arrPost["id"];
+        if (!preg_match('/^[a-zA-Z0-9_]{4,12}$/', $id)) {
+            $errMsg = "ID must be between 4 and 12 characters long and can only contain letters, numbers, and underscores";
+            $this->addDynamicProperty("errMsg", $errMsg);
+            return "signup" . _EXTENTION_PHP;
+        }
+
+        $pw = $arrPost["pw"];
+        if (!preg_match('/^.{8,20}$/', $pw)) {
+            $errMsg = "Password must be between 8 and 20 characters long";
+            $this->addDynamicProperty("errMsg", $errMsg);
+            return "signup" . _EXTENTION_PHP;
+        }
+
+        $pwchk = $arrPost["pwchk"];
+        if ($pw !== $pwchk) {
+            $errMsg = "Password does not match Password Check";
+            $this->addDynamicProperty("errMsg", $errMsg);
+            return "signup" . _EXTENTION_PHP;
+        }
+
+        $data = array(
+            'id' => $id,
+            'pw' => $pw,
+            'name' => $name
+        );
+        $this->model->setUser($data);
+        $errMsg = ": Successfully Signed Up. Please Login.";
+        $this->addDynamicProperty("errMsg", $errMsg);
+        return "login" . _EXTENTION_PHP;
+    }
+}
+
